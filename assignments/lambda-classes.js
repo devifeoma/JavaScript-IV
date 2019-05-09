087254b474cb7376d126662a0f040ed105e8bc14
Josemaria <josemarianriagu25@gmail.com>
2019-05-09 15:57:14 +0100

    - Complete stretch goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,6 +4,7 @@
 // * Person receives `name` `age` `location` `gender` all as props
 // * Person receives `speak` as a method.
 // * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+const school = 'Lambda School';
 
 class Person {
     constructor(name, age, location, gender) {
@@ -20,12 +21,12 @@ class Person {
 // Now that we have a Person as our base class, we'll build our Instructor class.
 // * Instructor uses the same attributes that have been set up by Person
 // * Instructor has the following unique props:
-//   * `specialty` what the Instructor is good at i.e. 'redux'
-//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-//   * `catchPhrase` i.e. `Don't forget the homies`
+// * `specialty` what the Instructor is good at i.e. 'redux'
+// * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+// * `catchPhrase` i.e. `Don't forget the homies`
 // * Instructor has the following methods:
-//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+// * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+// * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
 
 class Instructor extends Person {
     constructor(name, age, location, gender, specialty, favLanguage, catchPhrase) {
@@ -39,6 +40,16 @@ class Instructor extends Person {
     }
     grade(subject){
         return `${this.name} receives a perfect score on ${subject}`
+    }
+    doGrade(student, subject) {
+        let score = Math.floor(Math.random() * Math.floor(100));
+        if (score < 39) {
+            student.grade += score;
+            return `${student.name} receives ${score} on ${subject}`
+        } else {
+            student.grade += score;
+            return student.graduate();
+        }
     }
 }
 
@@ -59,20 +70,21 @@ console.log(gabe.grade('React.js'));
 // * Now we need some students!
 // * Student uses the same attributes that have been set up by Person
 // * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// * `previousBackground` i.e. what the Student used to do before Lambda School
+// * `className` i.e. CS132
+// * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
 // * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+// * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+// * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+// * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 class Student extends Person {
     constructor(name, age, location, gender, previousBackground, className, favSubjects) {
         super(name, age, location, gender),
         this.previousBackground = previousBackground,
         this.className = className,
-        this.favSubjects = favSubjects
+        this.favSubjects = favSubjects,
+        this.grade = 1
     }
     listsSubjects() {
         this.favSubjects.forEach(subject => console.log(subject))
@@ -90,6 +102,9 @@ class Student extends Person {
     }
     sprintChallenge(subject) {
         return `${this.name} has begun sprint challenge on ${subject}`
+    }
+    graduate() {
+        return `${this.name} has successfully graduated from ${school} with a final grade of ${this.grade}`
     }
 }
 
@@ -112,11 +127,11 @@ console.log(jose.sprintChallenge('JavaScript'));
 // * Now that we have instructors and students, we'd be nowhere without our PM's
 // * ProjectManagers are extensions of Instructors
 // * ProjectManagers have the following unique props:
-//   * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
+// * `gradClassName`: i.e. CS1
+// * `favInstructor`: i.e. Sean
 // * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+// * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+// * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
 
 class ProjectManager extends Instructor {
     constructor(name, age, location, gender, specialty, favLanguage, catchPhrase, gradClass, favInstructor) {
@@ -142,3 +157,18 @@ console.log(maxime.gradClass);
 console.log(maxime.favInstructor);
 console.log(maxime.standUp('webeu2_maxime'));
 console.log(maxime.debugsCode(jose,'Javascript'));
+
+
+
+
+// #### Stretch Problem
+
+// * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+// * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+// * Add a graduate method to a student.
+// * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+// * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+
+
+// Test stretch goal
+console.log(gabe.doGrade(jose, 'JS'));
